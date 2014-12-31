f4cf37dbf15033881742b2bc812c426e7df342bc
elarochelle <elarochelle@dyn.com>
2014-12-31 15:08:46 -0500

    Moving things around

--- demo_apps/wsgi.py
+++ demo_apps/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo_apps.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
