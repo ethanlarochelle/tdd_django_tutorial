928d6ae75c247a797c8006c7d606f2a55ac549c2
elarochelle <elarochelle@dyn.com>
2015-01-02 15:22:14 -0500

    Directory off by a level

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,12 +25,12 @@ def get_env_variable(var_name):
 settings_dir = os.path.dirname(os.path.dirname(__file__))
 BASE_DIR = os.path.abspath(os.path.join(settings_dir, '../../'))
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../media'))
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../assets'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'assets'))
 STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(BASE_DIR, '../assets')),
+    os.path.abspath(os.path.join(BASE_DIR, 'assets')),
 )
 TEMPLATE_DIRS = (
-    os.path.abspath(os.path.join(BASE_DIR, '../templates')),
+    os.path.abspath(os.path.join(BASE_DIR, 'templates')),
 )
 ALLOWED_HOSTS = []
 
